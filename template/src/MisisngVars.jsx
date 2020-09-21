fdce8c863285ea7e76667f2306a6a909734735de
Andoni Arostegui <andoni.arostegui@stacknvault.com>
2020-09-21 15:06:02 +0200

    Fixes
    - Make missing vars draggable

--- template/src/MisisngVars.jsx
+++ template/src/MisisngVars.jsx
@@ -1,22 +1,26 @@
 import React, { useContext } from 'react';
 import { ContextStore, getExternalConfig } from './lib/Context'
 import Box from '@material-ui/core/Box';
+import Draggable from 'react-draggable';
 
 function MissingVars() {
   const iexContext = useContext(ContextStore);
   const externalConfig = getExternalConfig();
   if (externalConfig && externalConfig.showMissingVars){
     return (
-
-      <Box borderRadius={16} style={{ padding: '15px', position: 'absolute', left: 20, top: 100, backgroundColor: 'black', color: 'white', opacity: 0.6}}>
-        <div style={{ margin: '0.1em', fontWeight: 'bold'}}>Missing variables</div><hr/>
-        {window.missingVars.map(item=><>
-          <div style={{ margin: '0.1em'}}>{item}</div><br/>
-        </>)}
+      <Draggable>
+      <Box borderRadius={16} style={{ cursor: 'move', padding: '15px', height: '50%', position: 'absolute', left: 20, top: 100, backgroundColor: 'black', color: 'white', opacity: 0.6}}>
+        <div style={{ margin: '0.1em', height: '10%', fontWeight: 'bold'}}>Fehlende Variablen</div><hr/>
+        <div style={{overflow: 'scroll', height: '90%'}}>
+          {window.missingVars.map(item=><>
+            <div style={{ margin: '0.1em'}}>{item}</div><br/>
+          </>)}
+        </div>
         {window.missingVars.length===0 && (<>
-          <div style={{ margin: '0.1em'}}>No missing variables</div><br/>
+          <div style={{ margin: '0.1em'}}>Kein Fehlende Variable</div><br/>
         </>)}
-      </Box>   
+      </Box> 
+      </Draggable>
     );
   }else{
     return (<></>);
